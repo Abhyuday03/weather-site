89a53e3a59035820d0f3308ac4dc8f379e1f2c6d
Abhyuday03 <abhyuday@kgpian.iitkgp.ac.in>
2021-12-06 18:57:24 +0530

    error corrected

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ const message_2=document.getElementById('message-2')
 weatherForm.addEventListener('submit',(e)=>{
   e.preventDefault()
   const location=search.value   
-  fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+  fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             message_1.innerHTML=data.error
