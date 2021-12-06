9636f867287cd91fad804dc2dc1fa0045f63bb19
Abhyuday03 <abhyuday@kgpian.iitkgp.ac.in>
2021-12-06 15:24:50 +0530

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+
+
+console.log('Client side javascript file is loaded!')
+
+const weatherForm=document.querySelector('form')
+const search=document.querySelector('input')
+const message_1=document.getElementById('message-1')
+const message_2=document.getElementById('message-2')
+
+weatherForm.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  const location=search.value   
+  fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            message_1.innerHTML=data.error
+            message_2.innerHTML=null
+
+        }
+        else{
+            message_1.innerHTML=data.location
+            message_2.innerHTML=data.forecast
+        }
+
+    })
+})
+})
